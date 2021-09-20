e430dfc827effb821499cda6ce367f57d8db1fd5
Red Wei <hevilhuy2@gmail.com>
2021-09-20 16:55:31 +0700

    Add dispose demonstration.

---
+++ ComponentLifecycleBlazorWasm/TransferServices/LifecycleDemonstrateTransferService.cs
@@ -0,0 +1,19 @@
+﻿namespace ComponentLifecycleBlazorWasm.TransferServices
+{
+    public class LifecycleDemonstrateTransferService
+    {
+        private string _log;
+        public event OnLogChangedHandle OnLogChanged;
+        public delegate void OnLogChangedHandle(object sender, string log);
+
+        public string Log
+        {
+            get => _log;
+            set
+            {
+                _log = value;
+                OnLogChanged?.Invoke(this, value);
+            }
+        }
+    }
+}
